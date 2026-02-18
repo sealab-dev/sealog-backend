36d0395ab83390171ca15f9a94c244fbac120e7b
Jh0-0y <idpc8090@gmail.com>
2026-02-19 00:31:19 +0900

    [feat] 환경 구축
    - 도커로 테스트 환경 구축
    - Redis 환경 추가
    - initlalizer 추가

---
+++ src/main/java/com/sealog/backend/global/initializer/LocalStackInitializer.java
@@ -0,0 +1,38 @@
+package com.sealog.backend.global.initializer;
+
+import com.sealog.backend.feature.stack.entity.Stack;
+import com.sealog.backend.feature.stack.entity.StackGroup;
+import com.sealog.backend.feature.stack.repository.StackRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@Profile("local")
+@RequiredArgsConstructor
+public class LocalStackInitializer implements ApplicationRunner {
+
+    private final StackRepository stackRepository;
+
+    @Override
+    public void run(ApplicationArguments args) {
+        createTestStack();
+    }
+
+    private void createTestStack() {
+        String name = "Test";
+        if (stackRepository.findByName(name).isEmpty()) {
+            Stack stack = Stack.builder()
+                    .name(name)
+                    .stackGroup(StackGroup.ETC)
+                    .build();
+
+            stackRepository.save(stack);
+            log.info("로컬 테스트 스택 생성: {}", name);
+        }
+    }
+}
