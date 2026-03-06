d6e47fe962f229f4815ea6b1eba7ad5adb360922
taeyoung9601 <rlaxoduddlqslek@naver.com>
2026-03-06 10:58:43 +0900

    [feat] User 통합테스트 생성

---
+++ src/test/java/com/sealog/backend/domain/feature/user/integration/UserIntegrationTest.java
@@ -0,0 +1,9 @@
+package com.sealog.backend.domain.feature.user.integration;
+
+import com.sealog.backend.support.base.TestIntegrationBase;
+import org.junit.jupiter.api.DisplayName;
+
+@DisplayName("User 통합 테스트 (Controller → Service → Repository)")
+class UserIntegrationTest extends TestIntegrationBase {
+
+}
