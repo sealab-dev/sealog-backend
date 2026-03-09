be634b06c066920ed8e481bae037fa987d8d5d22
Jh0-0y <idpc8090@gmail.com>
2026-03-09 12:35:50 +0900

    [Test] 테스트 컨테이너 방식 추가
    [feat] TestBase
    TestContainerBase : JVM이 컨테이너를 관리하는 방식
    TestContainerBaseV2 : 스프링이 컨테이너를 관리하는 방식

--- src/test/java/com/sealog/backend/domain/feature/user/integration/UserIntegrationTest.java
+++ src/test/java/com/sealog/backend/domain/feature/user/integration/UserIntegrationTest.java
@@ -19,7 +19,6 @@ import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
--- src/test/java/com/sealog/backend/support/base/TestPersistenceBase.java
+++ src/test/java/com/sealog/backend/support/base/TestPersistenceBase.java
@@ -1,14 +1,10 @@
 package com.sealog.backend.support.base;
 
-import com.sealog.backend.domain.feature.user.entity.User;
-import com.sealog.backend.domain.feature.user.enums.UserRole;
 import com.sealog.backend.support.extension.ExecutionTimeExtension;
 import com.sealog.backend.support.component.TestDataFactory;
 import com.sealog.backend.support.constant.TestMode;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.ActiveProfiles;
