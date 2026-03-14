0214da2c09ebd476efbf7afa2d42de8bd1d5b06c
jhy <idpc8090@gmail.com>
2026-03-14 16:01:34 +0900

    [feat] 소셜 타입 추가 Notion

--- src/main/java/com/sealog/backend/domain/feature/user/enums/SocialType.java
+++ src/main/java/com/sealog/backend/domain/feature/user/enums/SocialType.java
@@ -8,10 +8,11 @@ import lombok.RequiredArgsConstructor;
 public enum SocialType {
 
     GITHUB("github", "깃허브"),
+    NOTION("notion", "노션"),
+    PORTFOLIO("portfolio", "포트폴리오"),
     LINKEDIN("linkedin", "링크드인"),
     YOUTUBE("youtube", "유튜브"),
-    INSTAGRAM("instagram", "인스타그램"),
-    PORTFOLIO("portfolio", "포트폴리오");
+    INSTAGRAM("instagram", "인스타그램");
 
     private final String key;
     private final String title;
